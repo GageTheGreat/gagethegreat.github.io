9d7fac8e3da695465894ddce365f659c554c6b04
Reddstone35 <g.reddstone@gmail.com>
2022-03-16 16:56:35 -0600

    u

--- src/scripts/socials/yt.js
+++ src/scripts/socials/yt.js
@@ -1,4 +1,4 @@
-const youtubeKey = config.SECRET_API_KEY;
+const youtubeKey = "AIzaSyBNEUYJK5Tc3HjhQGIZkIOgzRPMCNU3ItU";
 const youtubeUser = 'UC0ouJNIXzJEvtOCW7K7WoOw';
 const subCount = document.getElementById('socials-yt-subCount');
 
