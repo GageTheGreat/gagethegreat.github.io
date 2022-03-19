62e3c655ac1dcf35f37315be667c5a1ee6f58c41
Reddstone35 <g.reddstone@gmail.com>
2022-03-19 11:34:18 -0600

    u

--- 15kyessir/src/scripts/main/index.js
+++ 15kyessir/src/scripts/main/index.js
@@ -2,7 +2,7 @@ window.onload = function(e) {
   if (sessionStorage.getItem("sidebarIsVisisble") === "false") {
     document.getElementById("sidebar").style.transitionDuration = "0ms"
     document.getElementById("sidebar").style.left  = "-260px";
-    document.getElementById("body").style.overflow = "auto"
+    document.getElementById("body").style.overflow = "overlay"
     setTimeout(() => {
       document.getElementById("sidebar").style.transitionDuration = "0.3s"
     }, 300);
@@ -24,7 +24,7 @@ document.getElementById("navlist-button").addEventListener("click", function(e) 
   } else if (sessionStorage.getItem("sidebarIsVisisble") === "true") {
     document.getElementById("sidebar").style.left  = "-260px";
     sessionStorage.setItem("sidebarIsVisisble", false)
-    document.getElementById("body").style.overflow = "auto"
+    document.getElementById("body").style.overflow = "overlay"
   } else {
     document.getElementById("sidebar").style.left  = "0px";
     sessionStorage.setItem("sidebarIsVisisble", true)
@@ -38,7 +38,7 @@ document.addEventListener('mouseup', function(e) {
   if (!container.contains(e.target) && !button.contains(e.target)) {
     document.getElementById("sidebar").style.left  = "-260px";
     sessionStorage.setItem("sidebarIsVisisble", false)
-    document.getElementById("body").style.overflow = "auto"
+    document.getElementById("body").style.overflow = "overlay"
   }
 });
 
