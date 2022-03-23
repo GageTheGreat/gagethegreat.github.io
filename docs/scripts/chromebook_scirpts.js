0565411defd461f43e0ee276730b9c7147c8aeb0
Reddstone35 <g.reddstone@gmail.com>
2022-03-22 19:38:10 -0600

    u

--- docs/scripts/chromebook_scirpts.js
+++ docs/scripts/chromebook_scirpts.js
@@ -1,7 +1,7 @@
-// #########################
-// # Chromebook JS scripts #
-// #     Made by Gage      #
-// #########################
+// ###############################
+// #    Chromebook JS scripts    #
+// # Made by Super Secret Person #
+// ###############################
 
 // dev tools console - unblocked
 function CheckForConsoleLog(txt) {
@@ -37,5 +37,24 @@ try {
     alert('You must type a valid problem. (ex. 5+5)');
   }
 }
+<<<<<<< Updated upstream
 //500 fake server error.
 document.write("<title>500 | Server Error</title><h1>500 - Internal Server Error</h1>The requested address 197.85.37.6 is not responding.")
+=======
+
+//fake web error - for making teachers think the website is broken.
+var errorCode = window.prompt("What error are you planning on triggering. (Enter error code):");
+if (errorCode == "400") {
+  document.write(`<title>400 | Bad Request</title><h1>400 - Bad Request</h1>The request sent to the host: ${window.location.hostname} was incorrect. You can try visiting this page at a later time.`);
+} else if (errorCode == "403") {
+  document.write(`<title>403 | Forbidden</title><h1>403 - Forbidden</h1>The web page: ${window.location.hostname + window.location.pathname} is not accessible. You can try visiting this page at a later time.`);
+} else if (errorCode == "404") {
+  document.write(`<title>404 | Not Found</title><h1>404 - Not Found</h1>The web page: ${window.location.hostname + window.location.pathname} was not found. You can try visiting this page at a later time.`);
+} else if (errorCode == "500") {
+  document.write(`<title>500 | Internal Server Error</title><h1>500 - Internal Server Error</h1>The server at host ${window.location.hostname} cannot handle this request. You can try visiting this page at a later time.`);
+} else if (errorCode == "429") {
+  document.write(`<title>429 | Too Many Requests</title><h1>429 - Too Many Requests</h1>You have sent too many requests to the server at host: ${window.location.hostname}. Wait a moment, and try again.`);
+} else {
+  alert("That code is not supported. Find supported codes at: https://reddstone35.com/redirect?token=DOC+tS-UMKsPfiftRcjYTEFpP5tyF34FhAwiv");
+}
+>>>>>>> Stashed changes
