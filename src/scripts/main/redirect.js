db824b13a1228d9912a306be16bccd488be51321
Reddstone35 <g.reddstone@gmail.com>
2022-04-06 21:10:50 -0600

    u

--- src/scripts/main/redirect.js
+++ src/scripts/main/redirect.js
@@ -6,13 +6,31 @@ function redirect(_href) {
   window.location = _href;
 }
 
+function download(fileUrl) {
+  var a = document.createElement("a");
+  a.href = fileUrl;
+  a.setAttribute("download", "");
+  a.click();
+  history.back()
+}
+
 function getFile(_token) {
   if (_token === "PlumTOS-YLi3M8Q4TXbe") {
     redirect("https://docs.google.com/document/d/1c0XF0HsGfmcMwNNdo5M1dJYgLZIft-yWYYiAA6RMPZo/view")
   } else if (_token === "DOC_tS-UMKsPfiftRcjYTEFpP5tyF34FhAwiv") {
     redirect("/docs/scripts/usage#chromebook-scripts_fake-error")
-  } else if (_token === "rNBnY1") {
-    redirect("/RennyBenni/")
+  } else if (_token === "img_d_pfp-red") {
+    download("/file/images/red_pfp.png")
+  } else if (_token === "img_d_pfp-orange") {
+    download("/file/images/orange_pfp.png")
+  } else if (_token === "img_d_pfp-yellow") {
+    download("/file/images/yellow_pfp.png")
+  } else if (_token === "img_d_pfp-green") {
+    download("/file/images/green_pfp.png")
+  } else if (_token === "img_d_pfp-blue") {
+    download("/file/images/blue_pfp.png")
+  } else if (_token === "img_d_pfp-purple") {
+    download("/file/images/purple_pfp.png")
   } else if (_token === null){
     redirect("/?error=REDIRECT_TOKEN_NOT_FOUND")
   } else {
