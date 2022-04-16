3e44f21e0f673d1cb2699739c510a4b8a3da7d7a
Reddstone35 <g.reddstone@gmail.com>
2022-04-15 19:05:12 -0600

    styles move + typestuff app

---
+++ apps/TypeStuff/index.js
@@ -0,0 +1,20 @@
+var input = document.querySelector('input'); // get the input element
+input.focus()
+input.addEventListener('input', resizeInput); // bind the "resizeInput" callback on "input" event
+resizeInput.call(input); // immediately call the function
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    input.value = ""
+    input.style.width = "0ch"
+  }
+})
+
+document.addEventListener("click", (e) => {
+if (e.target !== input)
+  input.focus()
+})
+
+function resizeInput() {
+  input.value.toLowerCase()
+  this.style.width = `100%`;
+}
