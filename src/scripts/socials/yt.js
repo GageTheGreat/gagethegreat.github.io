48b431d98e64833ef3c08cd1bb6d950d92624e04
Reddstone35 <55559772+Reddstone35@users.noreply.github.com>
2022-05-10 21:13:01 -0600

    new yt video

--- src/scripts/socials/yt.js
+++ src/scripts/socials/yt.js
@@ -23,7 +23,7 @@ function loadChannel(channelName) {
         getSubscribers("UC0ouJNIXzJEvtOCW7K7WoOw")
         subButton.setAttribute("href", "https://www.youtube.com/channel/UC0ouJNIXzJEvtOCW7K7WoOw?sub_confirmation=1")
         viewButton.setAttribute("href", "https://www.youtube.com/channel/UC0ouJNIXzJEvtOCW7K7WoOw")
-        videoMiniplayer.setAttribute("src", "https://www.youtube.com/embed/eEWzhKc81X4")
+        videoMiniplayer.setAttribute("src", "https://www.youtube.com/embed/FABa_Tti5UA")
         document.getElementById("reddstone35coding-channel-viewstats").style.backgroundColor = "rgba(214, 214, 214, 0.425)"
         document.getElementById("reddstone35-channel-viewstats").style.backgroundColor = "rgba(0, 0, 0, 0.225)"
     } else if (channelName === "reddstone35coding") {
