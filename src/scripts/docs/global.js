8acf411a5d4f1d43bb8208cacaf500bf8d398874
Reddstone35 <55559772+Reddstone35@users.noreply.github.com>
2022-05-14 16:52:37 -0600

    docs dark mode

---
+++ src/scripts/docs/global.js
@@ -0,0 +1,42 @@
+if (localStorage.getItem("docs-settings-dark") === null) {
+  localStorage.setItem("docs-settings-dark", "false")
+}
+
+var darkModeSwitch = document.getElementById("docs-settings-darkmode-switch")
+darkModeSwitch.addEventListener('change', (e) => {
+  if (darkModeSwitch.checked) {
+    docsDarkMode(true)
+  } else if (!darkModeSwitch.checked) {
+    docsDarkMode(false)
+  }
+})
+
+function docsDarkMode(on) {
+  if (on) {
+    document.getElementsByClassName("docs-section-main")[0].style.background = "#1f2226"
+    document.getElementsByClassName("docs-section-main")[0].style.color = "#ffffff"
+    document.querySelectorAll("hline")[0].style.borderColor = "#ffffff"
+    if (document.querySelectorAll(".code-aria")[0] !== undefined) {
+      document.querySelectorAll(".code-aria")[0].style.backgroundColor = "#15181a"
+    }
+
+    localStorage.setItem("docs-settings-dark", "true")
+  } else if (!on) {
+    document.getElementsByClassName("docs-section-main")[0].style.background = "#ffffff"
+    document.getElementsByClassName("docs-section-main")[0].style.color = "#000000"
+    document.querySelectorAll("hline")[0].style.borderColor = "#000000"
+    if (document.querySelectorAll(".code-aria")[0] !== undefined) {
+      document.querySelectorAll(".code-aria")[0].style.backgroundColor = "rgb(255, 238, 217)"
+    }
+    localStorage.setItem("docs-settings-dark", "false")
+  }
+}
+
+if (localStorage.getItem("docs-settings-dark") === "true") {
+  docsDarkMode(true)
+  darkModeSwitch.checked = true
+} else if (localStorage.getItem("docs-settings-dark") === "false") {
+  docsDarkMode(false)
+  darkModeSwitch.checked = false
+}
+console.log(localStorage.getItem("docs-settings-dark"))
