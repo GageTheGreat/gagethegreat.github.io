6fd27236d72ad75a98d594103e03dd5170d71042
Reddstone35 <55559772+Reddstone35@users.noreply.github.com>
2023-01-28 14:19:49 -0700

    mrewards u + modem

--- func/mrewards/index.js
+++ func/mrewards/index.js
@@ -13,14 +13,19 @@ function createRandString() {
   return randString
 }
 
-console.log(openWin)
-openTabsBtn.addEventListener("click", (e) => {
-  alert("Flash and lag warning. Do not continue if your pc is slow.")
-  for (var i = 0; i < 33; i++) {
+function createTabs() {
+  modemClose()
+  for (var i = 0; i < 40; i++) {
     openWin = window.open("https://www.bing.com/search?q=RewardsAuto_" + createRandString())
     if(openWin === null && i === 1 || !openWin && i === 1 || openWin.closed && i === 1 || typeof openWin.closed=='undefine+d' && i === 1) 
     { 
       alert("You need to enable popups to continue.")
     }
   }
+}
+
+console.log(openWin)
+openTabsBtn.addEventListener("click", (e) => {
+  modemShow("Flash and lag warning. Do not continue if your pc is slow. Click the x to abort.", "Continue", true, "createTabs()")
 })
+
--- src/scripts/main/index.js
+++ src/scripts/main/index.js
@@ -1,9 +1,31 @@
-// _fontSize = 17.6445
-// document.getElementById('docs-script-make-font-bigger-1').addEventListener("click", function(e) {
-//   _fontSize++
-//   document.getElementById("docs-script-aria-1").style.fontSize = `${_fontSize}px`
-// });
-// document.getElementById('docs-script-make-font-smaller-1').addEventListener("click", function(e) {
-//   _fontSize--
-//   document.getElementById("docs-script-aria-1").style.fontSize = `${_fontSize}px`
-// });
+var _modem_modem = document.getElementById("modem")
+var _dim_modem = document.getElementById("modem-dim")
+var _text_modem = document.getElementById("modem-text")
+var _button_modem = document.getElementById("modem-button")
+var _close_modem = document.getElementById("modem-close")
+
+_close_modem.addEventListener("click", () => {
+  modemClose()
+})
+
+function modemShow(text, buttonText, close_btn, continue_func) {
+  _text_modem.textContent = text;
+  _button_modem.innerText = buttonText;
+  _modem_modem.style.display = "unset";
+  _dim_modem.style.display = "unset";
+  if (close_btn === true) {
+    _close_modem.style.display = "unset"
+  }
+  _button_modem.addEventListener("click", (e) => {
+    eval(continue_func)
+  })
+  document.body.style.overflow = "hidden"
+}
+
+function modemClose() {
+  _modem_modem.style.display = "none";
+  _dim_modem.style.display = "none";
+  _text_modem.textContent = "";
+  _button_modem.innerText = "";
+  document.body.style.overflow = "overlay"
+}
