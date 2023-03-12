5437a9ce293088e169d80a645f4cc7de5602113f
Reddstone35 <55559772+Reddstone35@users.noreply.github.com>
2023-03-12 03:34:49 -0700

    Update jsinjector.js

--- file/largescripts/jsinjector.js
+++ file/largescripts/jsinjector.js
@@ -69,7 +69,7 @@ hr {
 }
 
 function makeDraggable() {
-  // credit w3schools.com
+  /* credit w3schools.com:*/
   dragElement(
     document.getElementById(
       '17c40bbff3ccfd961bfb36c373780e7809f3e80fed9e8426a9'
@@ -98,23 +98,23 @@ function makeDraggable() {
     function dragMouseDown(e) {
       e = e || window.event;
       e.preventDefault();
-      // get the mouse cursor position at startup:
+      /* get the mouse cursor position at startup: */
       pos3 = e.clientX;
       pos4 = e.clientY;
       document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
+      /* call a function whenever the cursor moves: */
       document.onmousemove = elementDrag;
     }
 
     function elementDrag(e) {
       e = e || window.event;
       e.preventDefault();
-      // calculate the new cursor position:
+      /* calculate the new cursor position: */
       pos1 = pos3 - e.clientX;
       pos2 = pos4 - e.clientY;
       pos3 = e.clientX;
       pos4 = e.clientY;
-      // set the element's new position:
+      /* set the element's new position: */
       elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
       elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
     }
