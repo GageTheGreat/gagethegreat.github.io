14dc82831d846c6e24cd6fe284d7e9d15748e47b
Reddstone35 <g.reddstone@gmail.com>
2024-05-28 03:19:49 -0600

    u

--- src/scripts/AntSim/index.js
+++ src/scripts/AntSim/index.js
@@ -1,8 +1,9 @@
-const progress = 60;
+const progress = 62;
 
 const barInner = document.querySelector('#bar-inner');
 const barOuter = document.querySelector('#bar-outer');
 const percentageView = document.querySelector('#percentage-done');
+const playNowBtn = document.querySelector('#play-now-btn')
 
 console.log(barInner);
 
@@ -10,6 +11,7 @@ barInner.style.width = `${progress}%`;
 
 if (progress >= 100) {
   percentageView.textContent = `${progress}% COMPLETE!`;
+  playNowBtn.style.display = "unset"
 } else {
   percentageView.textContent = `${progress}% Complete`;
 }
